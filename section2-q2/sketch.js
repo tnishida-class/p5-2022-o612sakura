2509ee6cea792383e5106b1835156c39061bcb16
o612sakura <114970494+o612sakura@users.noreply.github.com>
2022-10-18 13:25:52 +0900

    Change section2q-2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,14 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
-  }
+    if((i+j)%2==1)
+   {fill(130)}
+   else{fill(255)}
+    rect(i*size,j*size,25,25);// BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+    if((i+j)%2==1&&j<3){fill(255,0,0)}
+    if((i+j)%2==1&&j>4){fill(0)}
+    ellipse(i*size+12.5,j*size+12.5,21)
+}
+}
 }
